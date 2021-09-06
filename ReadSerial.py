b78df02aefa6227992cd3b0f2833609540ce4a58
HoldenJe <jeremy.holden@ontario.ca>
2021-09-06 19:33:11 -0400

    working ReadSerial

--- ReadSerial.py
+++ ReadSerial.py
@@ -1,11 +1,18 @@
 import serial
 
-marel = serial.Serial('COM7', 9600)
-print(marel.name)
+print("Define COM port. Example 'COM7':")
+mycom = input()
+print("Connecting to: " + mycom)
+
+marel = serial.Serial(mycom, 9600)
+print("Weigh object on scale and press record:")
 rl2 = marel.readline()
-print(rl2)
-
-
+rl2 = rl2.decode()
+rl2 = rl2.split("\t")
+weight = float(rl2[2])
+print("Your object weighs:")
+print(weight)
+print("")
 
 
 
